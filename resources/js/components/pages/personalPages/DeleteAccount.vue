c937d3137efd2c375a9b46ccac717ad6dd5a9a99
HiroKb <60965041+HiroKb@users.noreply.github.com>
2020-05-24 16:04:20 +0900

    update front アカウント削除ページのスタイリング

--- resources/js/components/pages/personalPages/DeleteAccount.vue
+++ resources/js/components/pages/personalPages/DeleteAccount.vue
@@ -1,30 +1,39 @@
 <template>
-    <div class="contents">
-        <form @submit.prevent="deleteAccount">
-            <p>アカウントを削除しますか？</p>
-            <button>削除</button>
-        </form>
-    </div>
+    <v-container class="py-12">
+        <v-card>
+            <v-card-title>アカウント削除</v-card-title>
+            <v-card-text>
+                <v-form ref="form" @submit.prevent="deleteAccount">
+                    <p>作成した個人スケジュール、共有カレンダーが全て削除されます。<br/>アカウントを削除しますか？</p>
+                    <v-btn block :color="mixinThemeColor" dark type="submit">削除</v-btn>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </v-container>
 </template>
 
 <script>
+    import colorsMixin from "../../../mixins/colorsMixin"
     import {mapState} from "vuex";
-
     export default {
         name: "DeleteAccount",
+        mixins: [colorsMixin],
         computed: {
             ...mapState({
                 apiStatus: state => state.user.apiStatus, // API通信成否
             })
         },
         methods: {
+            /**
+             * アカウント削除処理
+             * @return {Promise<void>}
+             */
             async deleteAccount() {
 
                 await this.$store.dispatch('user/deleteAccount')
 
-                // 通信成功時
                 if (this.apiStatus) {
-                    this.$router.push({name: 'welcome'})
+                    this.$router.push({name: 'authentication'})
                 }
             }
         }
@@ -32,5 +41,7 @@
 </script>
 
 <style scoped>
-
+    .container{
+        max-width: 600px;
+    }
 </style>
