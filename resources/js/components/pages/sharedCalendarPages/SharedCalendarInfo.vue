cc7d7d572d2b04a704e11d30a3105a31c34345b5
HiroKb <60965041+HiroKb@users.noreply.github.com>
2020-05-26 20:31:04 +0900

    update front 共有カレンダー情報・変更ページのスタイリング

--- resources/js/components/pages/sharedCalendarPages/SharedCalendarInfo.vue
+++ resources/js/components/pages/sharedCalendarPages/SharedCalendarInfo.vue
@@ -1,55 +1,93 @@
 <template>
-    <div>
-        <div class="contents">
-            <p>共有カレンダー名</p>
-            <p>{{ sharedCalendarData.calendar_name }}</p>
-            <button @click="showUpdateCalendarNameModal">変更</button>
-            <p>カレンダー検索ID/招待URL</p>
-            <p>{{ sharedCalendarData.search_id }}</p>
-            <p>{{ invitationUrl }}</p>
-            <button @click="showUpdateSearchIdModal">変更</button>
-        </div>
-        <div class="modal-background" v-show="modalFlg" @click="hideModal">
-            <div class="modal" >
-                <div class="modal-inner" @click.stop>
-
-                    <i class="fas fa-times" @click="hideModal"></i>
-                    <form @submit.prevent="updateCalendarName" v-show="updateCalendarNameForm.showFlg">
-                        <label for="calendar-name">新しい共有カレンダー名</label>
-                        <input id="calendar-name" type="text" v-model="updateCalendarNameForm.data.calendar_name">
-                        <p v-if="updateCalendarNameForm.errors.calendar_name">{{ updateCalendarNameForm.errors.calendar_name[0] }}</p>
-                        <button>変更</button>
-                    </form>
-                    <form @submit.prevent="updateSearchId" v-show="updateSearchIdForm.showFlg">
-                        <p>カレンダー検索ID</p>
-                        <p>{{ sharedCalendarData.search_id }}</p>
-                        <p>招待URL</p>
-                        <p>{{ invitationUrl }}</p>
-                        <p>カレンダー検索IDとカレンダー招待URLを変更しますか？</p>
-                        <button>変更</button>
-                    </form>
+    <v-container class="py-12">
+        <!--        共有カレンダー情報-->
+        <v-card>
+            <v-card-text class="pt-4">
+                <div class="py-4 d-flex align-end justify-space-between">
+                    <div>
+                        <p class="info-label">共有カレンダー名</p>
+                        <p class="info-data">{{ sharedCalendarData.calendar_name }}</p>
+                    </div>
+                    <v-btn
+                        :color="mixinThemeColor" dark
+                        @click.stop="editCalendarNameModal.show = !editCalendarNameModal.show"
+                    >変更</v-btn>
                 </div>
-            </div>
-        </div>
-    </div>
+                <v-divider></v-divider>
+                <div class="py-4 d-flex align-center justify-space-between">
+                    <div>
+                        <p class="info-label">検索ID</p>
+                        <p class="info-data">{{ sharedCalendarData.search_id }}</p>
+                        <p class="info-label mt-4">招待URL</p>
+                        <p class="info-data">{{ invitationUrl }}</p>
+                    </div>
+                    <v-btn
+                        :color="mixinThemeColor" dark
+                        @click.stop="updateSearchIdModal.show = !updateSearchIdModal.show"
+                    >変更</v-btn>
+                </div>
+            </v-card-text>
+        </v-card>
+
+<!--        カレンダー名変更モーダル-->
+        <v-dialog
+            v-model="editCalendarNameModal.show"
+            max-width="500px"
+        >
+            <v-card>
+                <v-card-title>カレンダー名変更</v-card-title>
+                <v-card-text>
+                    <v-form ref="editCalendarNameForm" @submit.prevent="updateCalendarName">
+                        <v-text-field
+                            v-model="editCalendarNameModal.form.calendar_name"
+                            label="新しいカレンダー名"
+                            :rules="[mixinValidationRules.required, mixinValidationRules.max100]"
+                            :error="editCalendarNameModal.errors ? !!editCalendarNameModal.errors.calendar_name : false"
+                            :error-messages="editCalendarNameModal.errors ? editCalendarNameModal.errors.calendar_name ? editCalendarNameModal.errors.calendar_name : [] : []"
+                            outlined
+                        ></v-text-field>
+                        <v-btn block :color="mixinThemeColor" dark type="submit">変更</v-btn>
+                    </v-form>
+                </v-card-text>
+            </v-card>
+        </v-dialog>
+
+<!--        検索ID変更モーダル-->
+        <v-dialog
+            v-model="updateSearchIdModal.show"
+            max-width="500px"
+        >
+            <v-card>
+                <v-card-title>カレンダー検索ID変更</v-card-title>
+                <v-card-text>
+                    <p>検索IDと招待URLがランダムに変更され、<br>現在の検索ID、招待URLは使用できなくなります。<p/>
+                    <v-form @submit.prevent="updateSearchId">
+                        <v-btn block :color="mixinThemeColor" dark type="submit">変更</v-btn>
+                    </v-form>
+                </v-card-text>
+            </v-card>
+        </v-dialog>
+    </v-container>
 </template>
 
 <script>
-    import {SUCCESS, VALIDATION_ERROR} from "../../../util";
+    import {SUCCESS, VALIDATION_ERROR} from "../../../util"
+    import colorsMixin from "../../../mixins/colorsMixin"
+    import validationRulesMixin from "../../../mixins/validationRulesMixin"
     export default {
         name: "SharedCalendarInfo.vue",
+        mixins: [validationRulesMixin, colorsMixin],
         data () {
             return {
-                modalFlg: false,
-                updateCalendarNameForm: {
-                    showFlg: false,
-                    data: {
+                editCalendarNameModal: {
+                    show: false,
+                    form: {
                         calendar_name: ''
                     },
                     errors: {}
                 } ,
-                updateSearchIdForm: {
-                    showFlg: false
+                updateSearchIdModal: {
+                    show: false
                 }
             }
         },
@@ -61,6 +99,9 @@
             }
         },
         computed: {
+            /**
+             * @return {String} 招待URL
+             */
             invitationUrl: function () {
                 return this.sharedCalendarData.search_id ?
                     location.protocol + '//' + location.host + '/personal/shared-calendar/' + this.sharedCalendarData.search_id + '/application'
@@ -68,71 +109,78 @@
             }
         },
         methods: {
+            /**
+             * カレンダー名変更処理
+             * @return {Promise<boolean>}
+             */
             async updateCalendarName() {
+                if (!this.$refs.editCalendarNameForm.validate()) {
+                    return false
+                }
+
                 this.$store.commit('loading/setLoadingFlg', true)
-                const response = await axios.patch('/api/shared-calendars/' + this.sharedCalendarData.id + '/name', this.updateCalendarNameForm.data)
+                const response = await axios.patch('/api/shared-calendars/' + this.sharedCalendarData.id + '/name', this.editCalendarNameModal.form)
                 this.$store.commit('loading/setLoadingFlg', false)
 
                 if (response.status === SUCCESS) {
                     this.$emit('changeCalendarData', response.data)
-                    this.hideModal()
+                    this.editCalendarNameModal.show = false
                     this.$store.commit('flashMessage/setMessage', '共有カレンダー名を変更しました')
                     return false
                 }
+
                 if (response.status === VALIDATION_ERROR) {
-                    this.updateCalendarNameForm.errors = response.data.errors
+                    this.editCalendarNameModal.errors = response.data.errors
                     return false
                 }
+
                 this.$store.commit('error/setCode', response.status)
             },
+            /**
+             * 検索ID変更処理
+             * @return {Promise<boolean>}
+             */
             async updateSearchId(){
+                if (!this.sharedCalendarData.id) {
+                    return false
+                }
+
                 this.$store.commit('loading/setLoadingFlg', true)
                 const response = await axios.patch('/api/shared-calendars/' + this.sharedCalendarData.id + '/search-id')
                 this.$store.commit('loading/setLoadingFlg', false)
 
                 if(response.status === SUCCESS) {
-
                     this.$emit('changeCalendarData', response.data)
-                    this.hideModal()
+                    this.updateSearchIdModal.show = false
                     this.$store.commit('flashMessage/setMessage', 'カレンダー検索ID/招待URLを変更しました')
                     return false
                 }
+
                 this.$store.commit('error/setCode', response.status)
             },
-            showUpdateCalendarNameModal() {
-                this.modalFlg = true;
-                this.updateCalendarNameForm.showFlg = true;
-            },
-            showUpdateSearchIdModal() {
-                this.modalFlg = true;
-                this.updateSearchIdForm.showFlg = true;
-            },
-            hideModal() {
-                this.modalFlg = false
+        },
+        watch: {
+            'editCalendarNameModal.show': function (val) {
+                if (!val) {
+                    this.editCalendarNameModal.form.calendar_name = ''
+                    this.$refs.editCalendarNameForm.resetValidation()
+                }
             }
         }
     }
 </script>
 
 <style scoped>
-    .modal-background{
-        position: fixed;
-        top: 0;
-        left: 0;
-        height: 100vh;
-        width: 100vw ;
-        z-index: 10;
-        background: rgba(0, 0, 0, .1);
+    .container{
+        max-width: 600px;
     }
-
-    .modal{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        width: 100vw ;
+    .info-label{
+        margin-bottom: 0;
     }
-    .modal-inner{
-        background: #ffffff;
+    .info-data{
+        margin-bottom: 0;
+        font-size: 20px;
+        word-wrap: break-word;
+        color: rgba(0, 0, 0 ,0.87);
     }
 </style>
