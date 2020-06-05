a6d3e09820bec9756e2a2013ea00ec886f855614
HiroKb <60965041+HiroKb@users.noreply.github.com>
2020-06-06 00:06:11 +0900

    fix back テストの修正

--- tests/Feature/UpdateUserImageApiTest.php
+++ tests/Feature/UpdateUserImageApiTest.php
@@ -9,7 +9,7 @@ use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
-class updateUserImageApiTest extends TestCase
+class UpdateUserImageApiTest extends TestCase
 {
     use RefreshDatabase;
 
