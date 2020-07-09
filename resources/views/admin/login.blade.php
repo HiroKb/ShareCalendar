0ee2f952ee0c8fac677e27d9617e0fb839e23cb4
HiroKb <60965041+HiroKb@users.noreply.github.com>
2020-07-10 02:18:19 +0900

    update front クローラー巡回拒否設定

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible"
           content="ie=edge">
+    <meta name="robots" content="noindex,nofollow">
     <link rel="icon" href="{{asset('/favicon.ico')}}">
     <title>{{ config('app.name') }}</title>
 </head>
